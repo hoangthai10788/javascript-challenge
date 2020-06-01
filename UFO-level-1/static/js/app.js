c8e0ca7da829d4b8548ac72007f3268bc707495c
hoangthai10788 <54550660+hoangthai10788@users.noreply.github.com>
2020-06-01 12:07:08 -0500

    Files Updated

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -4,14 +4,18 @@ var tableData = data;
 // YOUR CODE HERE!
 // Append table to the webpage and add new rows of data for each UFO sighting
 
-var tbody = d3.select("tbody");
-tableData.forEach((data) => {
-    var row = tbody.append("tr");
-    Object.entries(data).forEach(([key,value]) => {
-        var data = row.append("td");
-        data.text(value);
-    });
-});
+function table(data) {
+    var tbody = d3.select("tbody");
+    tbody.html("");
+
+    data.forEach((datarow) => {
+        var t_r = tbody.append("tr");
+        Object.entries(datarow).forEach(([key,value]) => {
+            var t_d = t_r.append("td");
+            t_d.text(value);
+        });
+    }); 
+}
 
 
 // Filter the data based on the date of interest
@@ -24,26 +28,14 @@ button.on("click", function() {
 
     var inputValue = inputElement.property("value");
 
-    console.log(inputValue);
+    var filteredData = tableData;
 
-    console.log(tableData);
-
-    var filteredData = tableData.filter(datetime => datetime.datetime === inputValue);
+    var filteredData = filteredData.filter(datetime => datetime.datetime === inputValue);
 
     console.log(filteredData);
 
-    var tbody = d3.select("tbody");
-
-    filteredData.forEach((record) => {
-        
-        var row = tbody.append("tr");
-
-        Object.entries(record).forEach(([key, value]) => {
-            var row_data = row.append("td");
-            row_data.text(value);
-        });
-    });
+    table(filteredData);
 });
 
-
+table(tableData);
 
